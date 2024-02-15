676b6da351139453d64157cf9ae8aca1fb0e0bb3
Debopriya Deb Roy <55049202+DDR13GIT@users.noreply.github.com>
2024-02-15 23:05:44 +0600

    Time: 1 ms (79.66%) | Memory: 44.2 MB (95.28%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int flag=1;
+        for(int i=1; i<nums.length;i++){
+            if(nums[i]!=nums[i-1]){
+                nums[flag]=nums[i];
+                flag++;
+            }
+        }
+        return flag;
+    }
+}
