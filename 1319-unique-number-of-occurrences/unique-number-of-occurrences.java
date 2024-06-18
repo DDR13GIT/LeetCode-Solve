8b35f2c1430d2aeca013ee327c2ed354ba5d5dd0
Debopriya Deb Roy <55049202+DDR13GIT@users.noreply.github.com>
2024-06-19 00:57:40 +0600

    Time: 3 ms (36.25%) | Memory: 41.6 MB (58.76%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> freq = new HashMap<>();
+
+        for (int i : arr) {
+            freq.put(i, freq.getOrDefault(i, 0) + 1);
+        }
+
+        Set<Integer> s = new HashSet<>();
+        for (int i : freq.values()) {
+            s.add(i);
+        }
+
+        return freq.size() == s.size();
+    }
+}
